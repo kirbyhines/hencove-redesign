dd5069008abf3e7de350cd7948ed7c526907823f
Kirby Hines <kirbyhines@Kirbys-MBP.local>
2022-03-14 12:39:32 -0400

    card fixes

--- pages/components/OfferCard.js
+++ pages/components/OfferCard.js
@@ -4,16 +4,16 @@ import React from 'react';
 
 function OfferCard({ title, image, description }) {
   return (
-    <div className=" py-3 px-2 border-2 rounded-lg border-orange border-opacity-30 hover:border-opacity-100 cursor-pointer text-blue font-medium shadow-md h-[500px] w-[90vw] md:w-[400px] lg:w-[500px] relative flex flex-col items-center hover:scale-105">
+    <div className=" py-3 px-2 border-2 rounded-lg border-orange border-opacity-30 hover:border-opacity-100 cursor-pointer text-blue font-medium shadow-md h-[525px] w-[90vw] md:w-[400px] lg:w-[500px] relative flex flex-col items-center hover:scale-105">
       <img
         src={image}
         alt=""
         className="object-cover h-4/5 absolute top-3 w-[95%] rounded-lg shadow-md"
       />
-      <h1 className="text-2xl absolute bottom-10 left-3">{title}</h1>
-      <p className=" opacity-70 font-light absolute bottom-5 left-3">
-        {description}
-      </p>
+      <div className=" absolute bottom-2 left-3">
+        <h1 className="text-2xl">{title}</h1>
+        <p className=" opacity-70 font-light">{description}</p>
+      </div>
     </div>
   );
 }
